1645168fa52aa860eb5cacb733a2c44233effa1a
Aiša Dervanović <59751382+dervanovicaisa@users.noreply.github.com>
2021-11-10 23:03:32 +0100

    zadaci

--- Zadatak1/methods.js
+++ Zadatak1/methods.js
@@ -1,19 +1,29 @@
 var getInputValue = document.querySelector('input[type="text"]');
 getInputValue.addEventListener("keydown", search);
 function search(e) {
-  let getAllItems = document.getElementsByClassName("list-group-item");
-  let result = document.getElementById("result");
+  let getAllItems = document.getElementsByClassName("itemgroup");
+  setCookie('Aisa', 20);
   for (let index = 0; index < getAllItems.length; index++) {
     if (getAllItems[index].innerHTML.toLowerCase().includes(e.target.value)) {
       getAllItems[index].style.display = "";
       getAllItems[index].addEventListener("click", (event) => {
         getInputValue.value = event.currentTarget.innerHTML;
+        getAllItems[index].style.display = "";
       });
     } else if (
       !getAllItems[index].innerHTML.toLowerCase().includes(e.target.value)
     ) {
       getAllItems[index].style.display = "none";
     }
-    }
   }
+}
 
+function setCookie(name,days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "="  + expires + "; path=/";
+}
